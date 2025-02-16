437fc7e5100c54cc522b34c50357de21a7ed4841
assajii <reyassaji@gmail.com>
2025-02-16 14:16:58 +0545

    ios configuration complete

--- QuizFronend/quiz_app/lib/screens/menu_drawer.dart
+++ QuizFronend/quiz_app/lib/screens/menu_drawer.dart
@@ -8,6 +8,13 @@ class MenuDrawer extends StatelessWidget {
       throw 'Could not launch $url';
     }
   }
+  void launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
   @override
   Widget build(BuildContext context) {
